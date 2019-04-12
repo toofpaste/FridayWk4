f3b133123673a77cef46d4d3b31cf3ac223ff9dd
Michael Larragueta <m.larragueta@yahoo.com>
2019-04-12 09:20:16 -0700

    add object and object array for multiple pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
+function Pizza(){
+  this.orders = [];
+  this.currentId = 0;
+};
+Pizza.prototype.addPizza = function(order){
+  order.id = this.assignId();
+  this.orders.push(order);
+};
+Pizza.prototype.assignId = function(){
+  this.currentId++;
+  return this.currentId;
+};
+
+function Order(size, topName, totalPrice){
+  this.size = size,
+  this.topName = topName,
+  this.totalPrice = totalPrice;
+};
+
+Order.prototype.fullOrder = function(){
+  return this.size + " with " + this.topName + ": $" + this.totalPrice
+};
+function printOrder(printPizza){
+  var pizzaList = $("#show-pizza");
+  var pizzaHtml = "";
+  printPizza.orders.forEach(function(order){
+    pizzaHtml += "<p id =" + order.id +">" + order.size + " " + order.topName + " " + order.totalPrice;
+  });
+pizzaList.html(pizzaHtml);
+};
+
+
 function plusMinus(){
   var num = 0;
   $("#size").text("Small");
@@ -34,17 +66,17 @@ function plusMinus(){
     };
   });
 };
-function printSize(num){
 
-}
 function submit(num){
   $("form#formOne").submit(function(event){
     event.preventDefault();
-    $("#print").text("$" + (num*5));
   });
 }
 
-
+var pizza = new Pizza();
 $(function(){
   plusMinus();
+  var newOrder = new Order("Large", "Pepperoni", 35);
+  pizza.addPizza(newOrder);
+  printOrder(pizza);
 });
